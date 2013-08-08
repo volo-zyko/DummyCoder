cdd4ef931ab7e5a72d18b1d45a33d128e982401a
Zolo Vyko <zolo.vyko@gmail.com>
2013-08-08 17:08:39 +0300

    Added more tests and refactored existing tests.

--- UT/test.sh
+++ UT/test.sh
@@ -4,7 +4,7 @@ COVERAGE_BEGIN_COMMAND=''
 COVERAGE_APPEND_COMMAND=''
 COVERAGE_REPORT_DIR='/tmp/dumco/dumco_cov/'
 
-print_help ()
+function print_help()
 {
     echo "Usage: $0 [-c [-d <dir>]]"
     echo " -c           generate coverage report"
@@ -31,6 +31,28 @@ do
     esac
 done
 
+function cont_s2c()
+{
+    run='../dumco.py'
+
+    inp="$1"
+    out="/tmp/dumco/$(basename "$inp")"
+    aux="$2"
+    mode="$3"
+
+    case "$mode" in
+        dumpxsd)
+            run="$run -i $inp $aux $mode -o $out"
+            ;;
+        rfilter)
+            run="$run -i $inp $aux $mode -o $out-C++ $4"
+            ;;
+    esac
+
+    echo "$run"
+    $COVERAGE_APPEND_COMMAND $run
+}
+
 cd "$(dirname "$BASH_SOURCE")"
 
 $COVERAGE_ERASE_COMMAND
@@ -38,32 +60,43 @@ $COVERAGE_ERASE_COMMAND
 $COVERAGE_BEGIN_COMMAND ../dumco.py -h
 $COVERAGE_APPEND_COMMAND ../dumco.py --version
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/OXMLTran2/ dumpxsd -o /tmp/dumco/OXMLTran2/
+cont_s2c UT/schemas/OXMLTran2/ '' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/OXMLTran3/ dumpxsd -o /tmp/dumco/OXMLTran3/
+cont_s2c UT/schemas/OXMLTran3/ '' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/OPC3/ dumpxsd -o /tmp/dumco/OPC3/
+cont_s2c UT/schemas/OPC3/ '' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/ODF12/ dumpxsd -o /tmp/dumco/ODF12/
+cont_s2c UT/schemas/ODF12/ '' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/XHTML10/ dumpxsd -o /tmp/dumco/XHTML10/
+cont_s2c UT/schemas/XHTML10/ '' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/FB20/ -n fb2 dumpxsd -o /tmp/dumco/FB20/
+cont_s2c UT/schemas/FB20/ '-n fb2' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/NewML12/ -n fb2 dumpxsd -o /tmp/dumco/NewML12/
+cont_s2c UT/schemas/NewML12/ '-n fb2' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/ebXML204/ -n fb2 dumpxsd -o /tmp/dumco/ebXML204/
+cont_s2c UT/schemas/ebXML204/ '-n fb2' dumpxsd
 
-$COVERAGE_APPEND_COMMAND \
-    ../dumco.py -i UT/schemas/Docbook50/ -n fb2 dumpxsd -o /tmp/dumco/Docbook50/
+cont_s2c UT/schemas/Docbook50/ '-n fb2' dumpxsd
+
+content_class='--context-class V::None::LoadContext'
+content_class_header='--context-class-header Formats/None/LoadContext.h'
+
+prefices='--uri-prefices http://schemas.openxmlformats.org/ urn:schemas-microsoft-com:'
+cont_s2c UT/schemas/OXMLTran3/ '' rfilter "$prefices $content_class $content_class_header"
+
+prefices='--uri-prefices http://schemas.openxmlformats.org/ http://purl.org/'
+cont_s2c UT/schemas/OPC3/ '' rfilter "$prefices $content_class $content_class_header"
+
+prefices='--uri-prefices urn:oasis:names:tc:'
+cont_s2c UT/schemas/ODF12/ '' rfilter "$prefices $content_class $content_class_header"
+
+# prefices='--uri-prefices http://www.w3.org/'
+# cont_s2c UT/schemas/XHTML10/ '' rfilter "$prefices $content_class $content_class_header"
+
+content_class='--context-class V::Fb2::LoadContext'
+content_class_header='--context-class-header Formats/Fb2/LoadContext.h'
+prefices='--uri-prefices http://www.gribuser.ru/'
+cont_s2c UT/schemas/FB20/ '-n fb2' rfilter "$prefices $content_class $content_class_header"
 
 if [ "$COVERAGE_BEGIN_COMMAND" != '' ]; then
     test -d "$COVERAGE_REPORT_DIR" && rm -rf "$COVERAGE_REPORT_DIR"
